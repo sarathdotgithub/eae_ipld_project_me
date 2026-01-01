abb86a75a865487bbed66a9b675702f7521c1956
sarathraj j <sarathrajjawahar@gmail.com>
2026-01-01 11:55:23 +0100

    completed home page

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ import base64
     
 # ----- Page configs (tab title, favicon) -----
 st.set_page_config(
-    page_title="<Your Name> Portfolio",
+    page_title="Sarathraj Jawahar Portfolio",
     page_icon="📊",
 )
 
@@ -16,31 +16,31 @@ def home_page():
         st.header("Introduction to Programming Languages for Data")
         st.write("###")
         st.write("***Final Project - Dec 2025***")
-        st.write("**Author:** <Your Name>")
+        st.write("**Author:** [Sarathrajj](https://github.com/sarathdotgithub)")
         st.write("**Instructor:** [Enric Domingo](https://github.com/enricd)")
 
 
     # ----- Top title -----
-    st.html("""<div style="text-align: center;"><h1 style="text-align: center;">👋 Hi! My name is Python</h1></div>""")  # TODO: Add your name
+    st.html("""<div style="text-align: center;"><h1 style="text-align: center;">👋 Hi! My name is Sarath</h1></div>""")  # TODO: Add your name
 
 
     # ----- Profile image file -----
-    profile_image_file_path = "profile.png"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
+    profile_image_file_path = "1234.jpg"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
 
     with open(profile_image_file_path, "rb") as img_file:
-        img = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
+        img = "data:image/jpg;base64," + base64.b64encode(img_file.read()).decode()
 
 
     # ----- Your Profile Image -----
     st.html(f"""
     <div style="display: flex; justify-content: center;">
-        <img src="{img}" alt="Your Name" width="300" height="300" style="border-radius: 50%; object-fit: cover; margin-top: 40px; margin-bottom: 40px;">
+        <img src="{img}" alt="Sarathraj Jawahar" width="450" height="525" style="border-radius: 50%; object-fit: cover; margin-top: 40px; margin-bottom: 40px;">
     </div>
     """)
 
 
     # ----- Personal title or short description -----
-    current_role = "Current Role and/or Studies or Description"   # TODO: Change this
+    current_role = "Data Analyst"   # TODO: Change this
 
     st.html(f"""<div style="text-align: center;"><h4><i>{current_role}</i></h4></div>""")
 
@@ -52,17 +52,17 @@ def home_page():
 
     # TODO: Modify and adapt the following lines to your info, you can add or remove some details if you want
     st.write("""
-    - 🧑‍💻 I am a <Current Role and/or Studies> 
+    - 🧑‍💻 I am studying Master's in Big Data and Analytics
 
-    - 🛩️ prev: <Previous Experience, Background or Studies>
+    - 🛩️ prev: Data Analyst, Mechanical engineer
 
-    - ❤️ <Your Passion and/or Interests>
+    - ❤️ sports,hiking
 
-    - 🤖 <Your Personal Projects>
+    - 🤖 https://github.com/sarathdotgithub/ADVENTURE-WORKS
 
-    - 🏂 <Your Hobbies>
+    - 🏂 Running,gym,bicing,Internationl affairs
 
-    - 📫 How to reach me: <Your Email>
+    - 📫 How to reach me: sjawahar@student.eae.es
 
     - 🏠 Barcelona
     """)
