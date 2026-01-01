9dc0ff181a2690a5b369923503f893be177d173f
sarathraj j <sarathrajjawahar@gmail.com>
2026-01-01 14:52:36 +0100

    error in the image_1

--- home.py
+++ home.py
@@ -25,7 +25,7 @@ def home_page():
 
 
     # ----- Profile image file -----
-    profile_image_file_path = "sarath_image_1.jpg"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
+    profile_image_file_path = "images\sarath_image_1.jpg"  # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
 
     with open(profile_image_file_path, "rb") as img_file:
         img = "data:image/jpg;base64," + base64.b64encode(img_file.read()).decode()
